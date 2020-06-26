d025f75e093ace9dd2670cd644d3cb9696dd898e
Håkan Wennlöf <hwennlof@gmail.com>
2020-06-26 08:06:24 +0000

    Update macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/detector_setup.h, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/DisplayOn.C, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/Fun4All_G4_FastMom.C, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/init_gui_vis.mac, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/Timing_GDML_.gdml, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/vis.mac, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/Vst_GDML_.gdml, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/Sitpc_GDML_.gdml, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/out_allSi_B_1.5T_FastTrackingEval.root, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/Fst_GDML_.gdml, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/Bst_GDML_.gdml, macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/SimpleNtuple.root files

---
+++ macros/withDisks/fixingRadiiReplacingTPC_combo_fancyDisks2/600/detector_setup.h
@@ -0,0 +1,38 @@
+//Beam pipe
+
+#define _BEAMPIPE_ //comment out this line for no beam pipe
+
+//Silicon Barrel
+
+#define _BARREL_ //comment out this line for no silicon barrel
+#define _NO_OF_BARREL_LAYERS_ 4
+
+//Timing layer
+
+#define _TIMING_
+#define _NO_OF_TIMING_LAYERS_ 1
+
+//Silicon TPC replacement
+
+#define _SITPC_ //comment out this line for no silicon barrel
+#define _NO_OF_SITPC_LAYERS_ 5
+
+//TPC
+
+//#define _TPC_ //comment out this line for no tpc
+
+
+//Silicon Disks
+
+#define _FORWARD_SILICON_DISKS_ //comment out this line for no silicon disks
+#define _NO_OF_FORWARD_DISKS_ 9
+
+#define _BACKWARD_SILICON_DISKS_ //comment out this line for no silicon disks
+#define _NO_OF_BACKWARD_DISKS_ 9
+
+//Black holes to catch all hits outside of colume
+
+//#define _BLACKHOLES_
+
+
+
